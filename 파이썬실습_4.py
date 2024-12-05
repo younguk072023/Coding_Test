2d608c57ce0a11810ed7e10a98ced8702b1370e6
young072023 <younguk072023@gmail.com>
2024-12-05 23:36:55 +0900

    실습 4

--- 파이썬실습_4.py
+++ 파이썬실습_4.py
@@ -4,37 +4,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# year=[2000,2005,2010,2015,2020]
-# GDP=[300,400,500,600,750]
+year=[2000,2005,2010,2015,2020]
+GDP=[300,400,500,600,750]
 
-# plt.plot(year,GDP,color='b',marker='o',linestyle='--')
+plt.plot(year,GDP,color='b',marker='o',linestyle='--')
 
-# plt.xlim(1995,2025)
-# plt.ylim(200,800)
+plt.xlim(1995,2025)
+plt.ylim(200,800)
 
-# plt.xlabel("Year")
-# plt.ylabel("GDP")
-# plt.title("GDP Graph")
-# plt.show()
+plt.xlabel("Year")
+plt.ylabel("GDP")
+plt.title("GDP Graph")
+plt.show()
 
 
-# #2 여러개의 데이터 시각화
+#2 여러개의 데이터 시각화
 
-# years=[2000,2005,2010,2015,2020]
-# kor_GDP=[300,400,500,600,700]
-# jap_GDP=[500,600,700,800,900]
-# chi_GDP=[100,200,400,800,1500]
+years=[2000,2005,2010,2015,2020]
+kor_GDP=[300,400,500,600,700]
+jap_GDP=[500,600,700,800,900]
+chi_GDP=[100,200,400,800,1500]
 
-# plt.plot(years,kor_GDP,color='r',marker='o',linestyle='--',label='korea')
-# plt.plot(years,jap_GDP,color='b',marker='*',linestyle='-',label='japen')
-# plt.plot(years,chi_GDP,color='g',marker='^',linestyle=':',label='china')
+plt.plot(years,kor_GDP,color='r',marker='o',linestyle='--',label='korea')
+plt.plot(years,jap_GDP,color='b',marker='*',linestyle='-',label='japen')
+plt.plot(years,chi_GDP,color='g',marker='^',linestyle=':',label='china')
 
-# plt.xlabel("Year")
-# plt.ylabel("GDP")
-# plt.legend()
-# plt.xticks(years)
-# plt.title("GDP Comparison")
-# plt.show()
+plt.xlabel("Year")
+plt.ylabel("GDP")
+plt.legend()
+plt.xticks(years)
+plt.title("GDP Comparison")
+plt.show()
 
 
 
