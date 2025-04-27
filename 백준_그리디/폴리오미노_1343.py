9c081465970981cf962d759c970ceebf9d94d5d7
young072023 <younguk072023@gmail.com>
2025-04-28 00:01:48 +0900

    오답

--- 백준_그리디/폴리오미노_1343.py
+++ 백준_그리디/폴리오미노_1343.py
@@ -2,29 +2,29 @@
 백준 폴리오미노 1343번 문제
 '''
 
-a=str(input())
+# a=str(input())
 
-part=a.split('.')
+# part=a.split('.')
 
-result=[]
+# result=[]
 
-for i in part:
+# for i in part:
 
-    temp=''
+#     temp=''
 
-    if len(i)%2!=0:
-        print(-1)
-        exit()
+#     if len(i)%2!=0:
+#         print(-1)
+#         exit()
     
-    while len(i)>=4:
-        temp+="AAAA"
-        i=i[4:]
-    while len(i)>=2:
-        temp+="BB"
-        i=i[2:]
-    result.append(temp)
+#     while len(i)>=4:
+#         temp+="AAAA"
+#         i=i[4:]
+#     while len(i)>=2:
+#         temp+="BB"
+#         i=i[2:]
+#     result.append(temp)
 
-print('.'.join(result))
+# print('.'.join(result))
 
 
 # join함수 문법
@@ -34,3 +34,35 @@ ex) reuslt=["BB","AAAA","AAAABB"]
 -> BB.AAAA.AAAABB
 이 문법을 모르면 이문제는 풀기 힘들다.
 '''
+
+
+a=str(input())
+
+part=a.split('.')
+
+result=[]   #문자열 넣을 리스트
+
+for i in part:
+    
+    temp='' #빈문자열 공간 생성
+
+    if len(i)%2!=0: #홀수일 경우 무조건 시스템 종료
+        print(-1)
+        exit()
+    
+    while len(i)>=4:
+        temp+='AAAA'
+        i=i[4:]
+    
+    while len(i)>=2:
+        temp+='BB'
+        i=i[2:]
+
+    result.append(temp)
+
+print('.'.join(result))
+
+
+    
+
+
