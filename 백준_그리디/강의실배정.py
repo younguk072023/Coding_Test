0f178f14340ced045308a97eb1c1984b20409c3a
younguk <younguk072023@gmail.com>
2026-03-09 13:30:42 +0900

    백준 1931번 문제

--- 백준_그리디/강의실배정.py
+++ 백준_그리디/강의실배정.py
@@ -8,27 +8,39 @@
 수강신청 대충한 게 찔리면, 선생님을 도와드리자!
 '''
 
-N = int(input())    #N개의 수업
+import sys
+import heapq
 
-list=[]
 
-for i in range(N):
-    S,T = map(int, input().split()) #시작과 끝 시간 입력
-    list.append((S,T))
+input = sys.stdin.readline 
+
+n = int(input())
+
+lectures = []
+for _ in range(n):
+    start, end = map(int, input().split())
+    lectures.append((start, end))
+
+lectures.sort()
+
+rooms = []
+
+if not lectures:
+    print(0)
+    exit()
+
+heapq.heappush(rooms, lectures[0][1])
+
+for i in range(1, n):
+    current_start, current_end = lectures[i]
     
-list.sort()
-print(list)
-# if N==1:
-#     print(1)
-# else:
-#     count=1
-#     end_time= list[0][1]
-#     for i in range(1,N):
-#         if end_time <=list[i][0]:
-#             count+=1
-#             end_time=list[i][1]
+    # 가장 빨리 끝나는 강의실을 재사용할 수 있는 경우
+    if current_start >= rooms[0]:
+        # heappop과 heappush를 heapreplace로 최적화
+        # 기존 원소를 pop하고 새로운 원소를 push하는 작업을 한 번에 수행
+        heapq.heapreplace(rooms, current_end)
+    else:
+        # 재사용 불가, 새로운 강의실 필요
+        heapq.heappush(rooms, current_end)
 
-# print(count)
-        
-
-    
+print(len(rooms))
